495a5b5dc2cbd517ce30fb19583d7ba08f51d548
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-02-09 13:32:13 +0000

    [maven-release-plugin]  copy for tag peppol-transport-api-2.2.0

---
+++ tags/peppol-transport-api/peppol-transport-api-2.2.0/src/main/java/at/peppol/transport/start/server/AccessPointReceiveError.java
@@ -0,0 +1,15 @@
+package at.peppol.transport.start.server;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import com.phloc.commons.log.InMemoryLogger;
+
+/**
+ * This class encapsulates the processing errors of an AccessPoint.
+ * 
+ * @author philip
+ */
+@NotThreadSafe
+public final class AccessPointReceiveError extends InMemoryLogger {
+  public AccessPointReceiveError () {}
+}
