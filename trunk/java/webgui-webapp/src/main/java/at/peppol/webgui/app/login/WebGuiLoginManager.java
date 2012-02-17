6dfbe918ac652f41a2cfa5d5b2a4775bb2d6564f
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-02-17 12:19:21 +0000

    Added error message on invalid login

--- trunk/java/webgui-webapp/src/main/java/at/peppol/webgui/app/login/WebGuiLoginManager.java
+++ trunk/java/webgui-webapp/src/main/java/at/peppol/webgui/app/login/WebGuiLoginManager.java
@@ -3,7 +3,12 @@ package at.peppol.webgui.app.login;
 import com.phloc.webbasics.app.html.IHTMLProvider;
 import com.phloc.webbasics.app.security.LoginManager;
 
-public class WebGuiLoginManager extends LoginManager {
+/**
+ * Override the default LoginManager referenced from the login servlet filter
+ * 
+ * @author philip
+ */
+public final class WebGuiLoginManager extends LoginManager {
   @Override
   protected IHTMLProvider createLoginScreen (final boolean bLoginError) {
     return new WebGuiLoginHTML (bLoginError);
