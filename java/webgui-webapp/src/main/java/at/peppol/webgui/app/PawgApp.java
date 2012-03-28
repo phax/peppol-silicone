3cfd151c953efbf3ba98d9cfe2bcba8d7e46ff2d
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-03-28 14:24:49 +0000

    Added StorageIO and GlobalDebug initialization in Listener

--- java/webgui-webapp/src/main/java/at/peppol/webgui/app/PawgApp.java
+++ java/webgui-webapp/src/main/java/at/peppol/webgui/app/PawgApp.java
@@ -1,67 +1,73 @@
 package at.peppol.webgui.app;
 
-import ap.peppol.webgui.security.user.IUser;
-import com.vaadin.Application;
-import com.vaadin.terminal.gwt.server.HttpServletRequestListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ap.peppol.webgui.security.user.IUser;
+
+import com.phloc.scopes.web.mgr.WebScopeManager;
+import com.vaadin.Application;
+import com.vaadin.terminal.gwt.server.HttpServletRequestListener;
+
 public class PawgApp extends Application implements HttpServletRequestListener {
 
-    private static ThreadLocal<PawgApp> threadLocal = new ThreadLocal<PawgApp>();
-    private IUser user;
+  private static ThreadLocal <PawgApp> threadLocal = new ThreadLocal <PawgApp> ();
+  private IUser user;
 
-    @Override
-    public void init() {
-        setInstance(this);
-        setTheme("peppol");
-        //showLoginWindow();
-        showMainAppWindow();
-    }
-    
-    private void showLoginWindow() {
-        LoginWindow win = new LoginWindow();
-        setMainWindow(win);
-    }
-    
-    private void showMainAppWindow() {
-        MainWindow mainWin = new MainWindow();
-        removeWindow(getMainWindow());
-        setMainWindow(mainWin);
-        //getMainWindow().open(new ExternalResource(mainWin.getURL()));
-        Logger.getLogger(PawgApp.class.getName()).log(Level.INFO,"Called showmain");
-    }
+  @Override
+  public void init () {
+    setInstance (this);
+    setTheme ("peppol");
+    // showLoginWindow();
+    showMainAppWindow ();
+  }
 
-    public static PawgApp getInstance() {
-        return threadLocal.get();
-    }
+  private void showLoginWindow () {
+    final LoginWindow win = new LoginWindow ();
+    setMainWindow (win);
+  }
 
-    // Set the current application instance 	
-    public static void setInstance(PawgApp application) {
-        threadLocal.set(application);
-    }
+  private void showMainAppWindow () {
+    final MainWindow mainWin = new MainWindow ();
+    removeWindow (getMainWindow ());
+    setMainWindow (mainWin);
+    // getMainWindow().open(new ExternalResource(mainWin.getURL()));
+    Logger.getLogger (PawgApp.class.getName ()).log (Level.INFO, "Called showmain");
+  }
 
-    @Override
-    public void onRequestStart(HttpServletRequest request, HttpServletResponse response) {
-        PawgApp.setInstance(this);
-    }
+  public static PawgApp getInstance () {
+    return threadLocal.get ();
+  }
 
-    @Override
-    public void onRequestEnd(HttpServletRequest request, HttpServletResponse response) {
-        threadLocal.remove();
-    }
+  // Set the current application instance
+  public static void setInstance (final PawgApp application) {
+    threadLocal.set (application);
+  }
 
-    public void authenticate(String username, String password) throws Exception {
-        
-        //Dummy Authentication
-        if (username.equals("peppol") && password.equals("peppol"))
-            showMainAppWindow();
-        
-        else 
-            throw new Exception("Login Failed");
-    
-    }
+  @Override
+  public void onRequestStart (final HttpServletRequest request, final HttpServletResponse response) {
+    PawgApp.setInstance (this);
+    WebScopeManager.onRequestBegin ("pawg", request, response);
+  }
+
+  @Override
+  public void onRequestEnd (final HttpServletRequest request, final HttpServletResponse response) {
+    WebScopeManager.onRequestEnd ();
+    threadLocal.remove ();
+  }
+
+  public void authenticate (final String username, final String password) throws Exception {
+
+    // Dummy Authentication
+    if (username.equals ("peppol") && password.equals ("peppol"))
+      showMainAppWindow ();
+
+    else
+      throw new Exception ("Login Failed");
+
+  }
 
 }
