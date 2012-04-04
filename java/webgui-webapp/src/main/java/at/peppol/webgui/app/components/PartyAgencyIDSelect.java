04c17421c4c8c1d6f9a243168b8ad9c9ed3a61cb
jerouris@gmail.com <jerouris@gmail.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-04-04 13:52:16 +0000

    Added a Combobox component that maps the Peppol Agency Codelist to a dropdown.

---
+++ java/webgui-webapp/src/main/java/at/peppol/webgui/app/components/PartyAgencyIDSelect.java
@@ -0,0 +1,38 @@
+package at.peppol.webgui.app.components;
+
+import at.peppol.commons.identifier.actorid.EPredefinedIdentifierIssuingAgency;
+import com.vaadin.ui.ComboBox;
+
+/**
+ *
+ * @author Jerouris
+ */
+public class PartyAgencyIDSelect extends ComboBox {
+    
+    public PartyAgencyIDSelect(String caption){
+        super(caption);
+        setWidth(13,UNITS_EM);
+        initData();
+    }
+    
+    private void initData() {
+      
+        for (EPredefinedIdentifierIssuingAgency a : EPredefinedIdentifierIssuingAgency.values()) {
+            addItem(a.getSchemeID());
+            setItemCaption(a.getSchemeID(),a.getSchemeAgency());
+        }
+        
+    }
+    
+    public String getSelectedAgencyName() 
+    {
+        return getItemCaption(getValue());
+    
+    }
+    
+    public String getSelectedAgencyID() 
+    {
+        return (String) getValue();
+    }
+    
+}
