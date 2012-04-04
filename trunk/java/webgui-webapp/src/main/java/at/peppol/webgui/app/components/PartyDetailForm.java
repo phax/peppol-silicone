276ce89d7b071eacc29554c8a6195a6a551c0157
jerouris@gmail.com <jerouris@gmail.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-04-04 13:51:13 +0000

    Added a Form Component that fills a PartyType UBL component. Useful both for the customer and supplier.

---
+++ trunk/java/webgui-webapp/src/main/java/at/peppol/webgui/app/components/PartyDetailForm.java
@@ -0,0 +1,113 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.peppol.webgui.app.components;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.util.NestedMethodProperty;
+import com.vaadin.data.util.PropertysetItem;
+import com.vaadin.ui.*;
+import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.*;
+import oasis.names.specification.ubl.schema.xsd.commonbasiccomponents_2.*;
+
+/**
+ *
+ * @author Jerouris
+ */
+public class PartyDetailForm extends Panel{
+
+    private final PartyType partyBean;
+    private final String party;
+    
+    public PartyDetailForm(String partyType, PartyType partyBean) {
+        this.party = partyType;
+        this.partyBean = partyBean;
+        
+        initElements();
+    
+    }
+        private void initElements() {
+
+        setCaption(party+" Details");
+        setStyleName("light");
+        
+        PropertysetItem partyItemSet = new PropertysetItem();
+        
+        final PartyIdentificationType supplierPartyID = new PartyIdentificationType();
+        supplierPartyID.setID(new IDType());
+        partyBean.getPartyIdentification().add(supplierPartyID);
+       
+        partyItemSet.addItemProperty("Party Id",
+                            new NestedMethodProperty(supplierPartyID, "ID.value"));
+        
+        final PartyNameType partyName = new PartyNameType();
+        partyName.setName(new NameType());
+        partyBean.getPartyName().add(partyName);
+        
+        partyItemSet.addItemProperty("Agency Name", 
+                new NestedMethodProperty(supplierPartyID, "ID.SchemeAgencyID") );
+        partyItemSet.addItemProperty("Party Name",
+                            new NestedMethodProperty(partyName, "name.value"));
+        
+        final AddressType partyrAddress = new AddressType();
+        partyBean.setPostalAddress(partyrAddress);
+        partyrAddress.setStreetName(new StreetNameType());
+        partyrAddress.setCityName(new CityNameType());
+        partyrAddress.setPostalZone(new PostalZoneType());
+        partyrAddress.setCountry(new CountryType());
+        
+        partyrAddress.getCountry().setIdentificationCode(new IdentificationCodeType());
+
+        partyItemSet.addItemProperty("Street Name",
+                            new NestedMethodProperty(partyrAddress, "streetName.value"));
+        partyItemSet.addItemProperty("City",
+                            new NestedMethodProperty(partyrAddress, "cityName.value"));
+        partyItemSet.addItemProperty("Postal Zone",
+                            new NestedMethodProperty(partyrAddress, "postalZone.value"));
+        partyItemSet.addItemProperty("Country",
+                            new NestedMethodProperty(partyrAddress, "country.identificationCode.value"));
+
+
+        final Form partyForm = new Form();
+        partyForm.setFormFieldFactory(new PartyFieldFactory());
+        partyForm.setItemDataSource(partyItemSet);
+        partyForm.setImmediate(true);
+        
+        addComponent(partyForm);
+
+        
+
+    }
+    
+     class PartyFieldFactory implements FormFieldFactory {
+
+        @Override
+        public Field createField(Item item, Object propertyId,
+                Component uiContext) {
+            // Identify the fields by their Property ID.
+            String pid = (String) propertyId;
+            if ("Agency Name".equals(pid)) {
+                final PartyAgencyIDSelect select = new PartyAgencyIDSelect("Agency Name");
+                select.addListener(new Property.ValueChangeListener() {
+
+                    @Override
+                    public void valueChange(com.vaadin.data.Property.ValueChangeEvent event) {
+                        partyBean.getPartyIdentification().get(0)
+                                .getID()
+                                .setSchemeAgencyName(select.getSelectedAgencyName());
+                    }
+                });
+                return select;
+            }
+
+            Field field = DefaultFieldFactory.get().createField(item,propertyId, uiContext);
+            if (field instanceof AbstractTextField){
+                ((AbstractTextField) field).setNullRepresentation("");
+            }
+            
+            return field;
+        }
+     }
+}
