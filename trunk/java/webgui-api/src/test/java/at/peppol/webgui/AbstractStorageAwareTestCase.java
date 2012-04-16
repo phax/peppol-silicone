ec29c4d657eff6dfb37aa5af3a3f25fed8134fc9
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-04-16 11:36:13 +0000

    Added test

--- trunk/java/webgui-api/src/test/java/at/peppol/webgui/AbstractStorageAwareTestCase.java
+++ trunk/java/webgui-api/src/test/java/at/peppol/webgui/AbstractStorageAwareTestCase.java
@@ -4,11 +4,14 @@ import java.io.File;
 
 import at.peppol.webgui.io.StorageIO;
 
+import com.phloc.commons.idfactory.FileIntIDFactory;
+import com.phloc.commons.idfactory.GlobalIDFactory;
 import com.phloc.scopes.web.mock.AbstractWebScopeAwareTestCase;
 
 public abstract class AbstractStorageAwareTestCase extends AbstractWebScopeAwareTestCase {
   static {
     // Init the base path once
     StorageIO.initBasePath (new File ("target/junit").getAbsolutePath ());
+    GlobalIDFactory.setPersistentIntIDFactory (new FileIntIDFactory (StorageIO.getFile ("ids.dat")));
   }
 }
