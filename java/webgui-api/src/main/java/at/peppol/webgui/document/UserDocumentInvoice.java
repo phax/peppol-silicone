df71e5ef1bd38b4c314311cd422cbf290946c762
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-04-16 14:39:58 +0000

    Invoice document

---
+++ java/webgui-api/src/main/java/at/peppol/webgui/document/UserDocumentInvoice.java
@@ -0,0 +1,33 @@
+package at.peppol.webgui.document;
+
+import javax.annotation.Nonnull;
+
+import oasis.names.specification.ubl.schema.xsd.invoice_2.InvoiceType;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.idfactory.GlobalIDFactory;
+
+/**
+ * Represents a single invoice document
+ * 
+ * @author philip
+ */
+public final class UserDocumentInvoice extends AbstractUserDocument {
+  private final InvoiceType m_aInvoice;
+
+  public UserDocumentInvoice (@Nonnull final InvoiceType aInvoice) {
+    this (GlobalIDFactory.getNewPersistentStringID (), aInvoice);
+  }
+
+  public UserDocumentInvoice (@Nonnull @Nonempty final String sID, @Nonnull final InvoiceType aInvoice) {
+    super (sID, EDocumentType.INVOICE);
+    if (aInvoice == null)
+      throw new NullPointerException ("invoice");
+    m_aInvoice = aInvoice;
+  }
+
+  @Nonnull
+  public InvoiceType getInvoice () {
+    return m_aInvoice;
+  }
+}
