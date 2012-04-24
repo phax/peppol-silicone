2e6914987bc07fee28c43eb92e187b5b2c048c90
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-04-24 10:41:30 +0000

    Using correct handlers

--- java/webgui-webapp/src/main/java/at/peppol/webgui/app/PawgApp.java
+++ java/webgui-webapp/src/main/java/at/peppol/webgui/app/PawgApp.java
@@ -1,6 +1,5 @@
 package at.peppol.webgui.app;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,104 +16,107 @@ import com.vaadin.Application;
 import com.vaadin.terminal.gwt.server.HttpServletRequestListener;
 import com.vaadin.ui.Window;
 
-
 public class PawgApp extends Application implements HttpServletRequestListener {
 
-    private static ThreadLocal<PawgApp> threadLocal = new ThreadLocal<PawgApp>();
-    private static final Logger LOGGER = LoggerFactory.getLogger(PawgApp.class);
-    private IUser user;
-    private final LoggedInUserManager lum = LoggedInUserManager.getInstance();
+  private static final ThreadLocal <PawgApp> threadLocal = new ThreadLocal <PawgApp> ();
+  private static final Logger LOGGER = LoggerFactory.getLogger (PawgApp.class);
+  private IUser user;
+  private final LoggedInUserManager lum = LoggedInUserManager.getInstance ();
 
-    @Override
-    public void init() {
-        // Ensure that no user is logged in
-        setInstance(this);
-        setTheme("peppol");  
-        try {
-        lum.logoutCurrentUser();
-        //showLoginWindow();
-        // startWithMainWindow();
-        authenticate("user@peppol.eu", "user");
-        } catch (Exception ex) {
-            LOGGER.error(null, ex);
-        }
+  @Override
+  public void init () {
+    // Ensure that no user is logged in
+    setInstance (this);
+    setTheme ("peppol");
+    try {
+      lum.logoutCurrentUser ();
+      if (false) {
+        showLoginWindow ();
+        startWithMainWindow ();
+      }
+      else {
+        authenticate ("user@peppol.eu", "user");
+      }
+    }
+    catch (final Exception ex) {
+      LOGGER.error (null, ex);
+    }
+  }
+
+  private void showLoginWindow () {
+    final LoginWindow win = new LoginWindow ();
+    setMainWindow (win);
+  }
+
+  private void showMainAppWindow () {
+    final MainWindow mainWin = new MainWindow ();
+    if (getMainWindow () != null)
+      removeWindow (getMainWindow ());
+
+    setMainWindow (mainWin);
+    // getMainWindow().open(new ExternalResource(mainWin.getURL()));
+    LOGGER.debug ("Called Showmain");
+  }
+
+  public static PawgApp getInstance () {
+    return threadLocal.get ();
+  }
+
+  // Set the current application instance
+  public static void setInstance (final PawgApp application) {
+    threadLocal.set (application);
+  }
+
+  @Override
+  public void onRequestStart (final HttpServletRequest request, final HttpServletResponse response) {
+    PawgApp.setInstance (this);
+    WebScopeManager.onRequestBegin ("pawg", request, response);
+  }
+
+  @Override
+  public void onRequestEnd (final HttpServletRequest request, final HttpServletResponse response) {
+    WebScopeManager.onRequestEnd ();
+    threadLocal.remove ();
+  }
+
+  public void authenticate (final String username, final String password) throws Exception {
+
+    final ELoginResult res = lum.loginUser (username, password);
+
+    if (res.isSuccess ()) {
+      user = AccessManager.getInstance ().getUserOfID (lum.getCurrentUserID ());
+      System.out.println ("USER: " + user.getDisplayName ());
+      setUser (user);
+      showMainAppWindow ();
+
+    }
+    else {
+      throw new Exception (res.toString ());
     }
 
-    private void showLoginWindow() {
-        final LoginWindow win = new LoginWindow();
-        setMainWindow(win);
+  }
+
+  public void logout () {
+    lum.logoutCurrentUser ();
+    close ();
+  }
+
+  private void startWithMainWindow () throws Exception {
+    user = AccessManager.getInstance ().getUserOfEmailAddress ("user@peppol.eu");
+    setUser (user);
+    showMainAppWindow ();
+  }
+
+  @Override
+  public Window getWindow (final String name) {
+    Window w = super.getWindow (name);
+    if (w == null) {
+      w = new MainWindow (); // it's best to have separate classes for your
+                             // windows
+      w.setName (name);
+      addWindow (w);
     }
+    return w;
+  }
 
-    private void showMainAppWindow() {
-        final MainWindow mainWin = new MainWindow();
-        if (getMainWindow()!= null)
-            removeWindow(getMainWindow());
-    
-        setMainWindow(mainWin);
-        // getMainWindow().open(new ExternalResource(mainWin.getURL()));
-        LOGGER.debug("Called Showmain");
-    }
-
-    public static PawgApp getInstance() {
-        return threadLocal.get();
-    }
-
-    // Set the current application instance
-    public static void setInstance(final PawgApp application) {
-        threadLocal.set(application);
-    }
-
-    @Override
-    public void onRequestStart(final HttpServletRequest request, final HttpServletResponse response) {
-        PawgApp.setInstance(this);
-        WebScopeManager.onRequestBegin("pawg", request, response);
-    }
-
-    @Override
-    public void onRequestEnd(final HttpServletRequest request, final HttpServletResponse response) {
-        WebScopeManager.onRequestEnd();
-        threadLocal.remove();
-    }
-
-    public void authenticate(final String username, final String password) throws Exception {
-
-        final ELoginResult res = lum.loginUser(username, password);
-
-        if (res.isSuccess()) {
-            user = AccessManager.getInstance().getUserOfID(lum.getCurrentUserID());
-            System.out.println("USER: "+user.getDisplayName());
-            setUser(user);
-            showMainAppWindow();
-
-
-        } else {
-            throw new Exception(res.toString());
-        }
-
-    }
-
-    public void logout() {
-        lum.logoutCurrentUser();
-        close();
-    }
-    
-    private void startWithMainWindow() throws Exception {
-        user = AccessManager.getInstance()
-                            .getUserOfEmailAddress("user@peppol.eu");
-            setUser(user);
-            showMainAppWindow();
-    }
-    
-    @Override
-    public Window getWindow(String name) {
-        Window w = super.getWindow(name);
-        if (w == null) {
-                w = new MainWindow(); // it's best to have separate classes for your windows
-                w.setName(name);
-                addWindow(w);
-        }
-        return w;
-    }
-    
-   
 }
