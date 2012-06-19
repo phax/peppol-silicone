6735aad09569f9898b5e519791a7132a0c0b9ce3
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-06-19 12:54:22 +0000

    Some reuse

--- trunk/java/oxalis-commons/src/main/java/eu/peppol/start/identifier/KeystoreManager.java
+++ trunk/java/oxalis-commons/src/main/java/eu/peppol/start/identifier/KeystoreManager.java
@@ -1,9 +1,6 @@
 package eu.peppol.start.identifier;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.Key;
 import java.security.KeyStore;
@@ -21,215 +18,199 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import at.peppol.commons.security.KeyStoreUtils;
 import eu.peppol.security.OcspValidatorCache;
 
 /**
  * Main manager for handling operations related to our keystore and truststore.
  * <p/>
- * User: nigel
- * Date: Oct 9, 2011
- * Time: 4:01:31 PM
- *
+ * User: nigel Date: Oct 9, 2011 Time: 4:01:31 PM
+ * 
  * @author steinar@sendregning.no
  */
 public class KeystoreManager {
 
-    private static String keystoreLocation;
-    private static String keystorePassword;
-    private static KeyStore keyStore;
-    private static KeyStore trustStore;
-    private static PrivateKey privateKey;
-    private CertPathValidator certPathValidator;
-    private OcspValidatorCache ocspValidatorCache = new OcspValidatorCache();
-    private PKIXParameters pkixParameters;
+  private static String keystoreLocation;
+  private static String keystorePassword;
+  private static KeyStore keyStore;
+  private static KeyStore trustStore;
+  private static PrivateKey privateKey;
+  private CertPathValidator certPathValidator;
+  private final OcspValidatorCache ocspValidatorCache = new OcspValidatorCache ();
+  private PKIXParameters pkixParameters;
 
-    public KeystoreManager() {
-        initCertPathValidator();
+  public KeystoreManager () {
+    initCertPathValidator ();
+  }
+
+  public synchronized KeyStore getKeystore () {
+    if (keyStore == null)
+      keyStore = getKeystore (keystoreLocation, keystorePassword);
+    return keyStore;
+  }
+
+  private KeyStore getKeystore (final String location, final String password) {
+    try {
+      return KeyStoreUtils.loadKeyStore (location, password);
+    }
+    catch (final Exception e) {
+      throw new RuntimeException ("Failed to open keystore " + location, e);
+    }
+  }
+
+  public X509Certificate getOurCertificate () {
+    try {
+      final KeyStore keystore = getKeystore ();
+      final String alias = keystore.aliases ().nextElement ();
+      return (X509Certificate) keystore.getCertificate (alias);
+    }
+    catch (final KeyStoreException e) {
+      throw new RuntimeException ("Failed to get our certificate from keystore", e);
+    }
+  }
+
+  public synchronized PrivateKey getOurPrivateKey () {
+    if (privateKey == null) {
+      try {
+
+        final KeyStore keystore = getKeystore ();
+        final String alias = keystore.aliases ().nextElement ();
+        final Key key = keystore.getKey (alias, keystorePassword.toCharArray ());
+
+        if (key instanceof PrivateKey) {
+          privateKey = (PrivateKey) key;
+        }
+        else {
+          throw new RuntimeException ("Private key is not first element in keystore at " + keystoreLocation);
+        }
+
+      }
+      catch (final Exception e) {
+        throw new RuntimeException ("Failed to get our private key", e);
+      }
     }
 
-    public synchronized KeyStore getKeystore() {
-        if (keyStore == null) {
-            keyStore = getKeystore(keystoreLocation, keystorePassword);
-        }
+    return privateKey;
+  }
 
-        return keyStore;
+  public TrustAnchor getTrustAnchor () {
+
+    try {
+
+      final KeyStore truststore = getTruststore ();
+      final String alias = "ap";
+      return new TrustAnchor ((X509Certificate) truststore.getCertificate (alias), null);
+
+    }
+    catch (final Exception e) {
+      throw new RuntimeException ("Failed to get the PEPPOL access point certificate", e);
+    }
+  }
+
+  public synchronized KeyStore getTruststore () {
+    if (trustStore == null)
+      trustStore = getKeystore ("/truststore/global-truststore.jks", "peppol");
+    return trustStore;
+  }
+
+  public void initialiseKeystore (final File keystoreFile, final String keystorePassword) {
+    if (keystoreFile == null) {
+      throw new IllegalStateException ("Keystore file not specified");
     }
 
-    private KeyStore getKeystore(String location, String password) {
-
-        try {
-
-            return getKeystore(new FileInputStream(location), password);
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Failed to open keystore " + location, e);
-        }
+    if (keystorePassword == null) {
+      throw new IllegalStateException ("Keystore password not specified");
     }
 
-    private KeyStore getKeystore(InputStream inputStream, String password) {
-        try {
-
-            KeyStore keyStore = KeyStore.getInstance("JKS");
-            keyStore.load(inputStream, password.toCharArray());
-            return keyStore;
-
-        } catch (Exception e) {
-
-            throw new RuntimeException("Failed to open keystore", e);
-
-        } finally {
-            try {
-                inputStream.close();
-            } catch (Exception e) {
-            }
-        }
+    if (!keystoreFile.exists ()) {
+      throw new IllegalStateException ("Keystore file " + keystoreFile + " does not exist");
     }
 
-    public X509Certificate getOurCertificate() {
+    try {
 
-        try {
-            KeyStore keystore = getKeystore();
-            String alias = keystore.aliases().nextElement();
-            return (X509Certificate) keystore.getCertificate(alias);
+      setKeystoreLocation (keystoreFile.getCanonicalPath ());
+      setKeystorePassword (keystorePassword);
+      getKeystore ();
 
-        } catch (KeyStoreException e) {
-            throw new RuntimeException("Failed to get our certificate from keystore", e);
-        }
+    }
+    catch (final Exception e) {
+      throw new IllegalArgumentException ("Problem accessing keystore file", e);
+    }
+  }
+
+  void initCertPathValidator () {
+    Log.debug ("Initialising OCSP validator");
+
+    try {
+      final TrustAnchor trustAnchor = getTrustAnchor ();
+      certPathValidator = CertPathValidator.getInstance ("PKIX");
+      pkixParameters = new PKIXParameters (Collections.singleton (trustAnchor));
+      pkixParameters.setRevocationEnabled (true);
+
+      Security.setProperty ("ocsp.enable", "true");
+      Security.setProperty ("ocsp.responderURL", "http://pilot-ocsp.verisign.com:80");
+
+    }
+    catch (final Exception e) {
+      throw new IllegalStateException ("Unable to construct Certificate Path Validator; " + e, e);
+    }
+  }
+
+  /**
+   * Validates a X509 certificate using the OCSP services.
+   * 
+   * @param certificate
+   *        the certificate to be checked for validity
+   * @return <code>true</code> if certificate is valid, <code>false</code>
+   *         otherwise
+   */
+  public synchronized boolean validate (final X509Certificate certificate) {
+
+    final BigInteger serialNumber = certificate.getSerialNumber ();
+    final String certificateName = "Certificate " + serialNumber;
+    Log.debug ("Ocsp validation requested for " +
+               certificateName +
+               "\n\tSubject:" +
+               certificate.getSubjectDN () +
+               "\n\tIssued by:" +
+               certificate.getIssuerDN ());
+
+    if (certPathValidator == null) {
+      throw new IllegalStateException ("Certificate Path validator not initialized");
     }
 
-    public synchronized PrivateKey getOurPrivateKey() {
-
-        if (privateKey == null) {
-            try {
-
-                KeyStore keystore = getKeystore();
-                String alias = keystore.aliases().nextElement();
-                Key key = keystore.getKey(alias, keystorePassword.toCharArray());
-
-                if (key instanceof PrivateKey) {
-                    privateKey = (PrivateKey) key;
-                } else {
-                    throw new RuntimeException("Private key is not first element in keystore at " + keystoreLocation);
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException("Failed to get our private key", e);
-            }
-        }
-
-        return privateKey;
+    if (ocspValidatorCache.isKnownValidCertificate (serialNumber)) {
+      Log.debug (certificateName + " is OCSP valid (cached value)");
+      return true;
     }
 
-    public TrustAnchor getTrustAnchor() {
+    try {
 
-        try {
+      final List <Certificate> certificates = Arrays.asList (new Certificate [] { certificate });
+      final CertPath certPath = CertificateFactory.getInstance ("X.509").generateCertPath (certificates);
+      certPathValidator.validate (certPath, pkixParameters);
+      ocspValidatorCache.setKnownValidCertificate (serialNumber);
 
-            KeyStore truststore = getTruststore();
-            String alias = "ap";
-            return new TrustAnchor((X509Certificate) truststore.getCertificate(alias), null);
+      Log.debug (certificateName + " is OCSP valid");
+      return true;
 
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get the PEPPOL access point certificate", e);
-        }
     }
-
-    public synchronized KeyStore getTruststore() {
-        if (trustStore == null) {
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("truststore.jks");
-            trustStore = getKeystore(inputStream, "peppol");
-        }
-
-        return trustStore;
+    catch (final Exception e) {
+      Log.error (certificateName + " failed OCSP validation", e);
+      return false;
     }
+  }
 
-    public void initialiseKeystore(File keystoreFile, String keystorePassword) {
-        if (keystoreFile == null) {
-            throw new IllegalStateException("Keystore file not specified");
-        }
+  public boolean isOurCertificate (final X509Certificate candidate) {
+    final X509Certificate ourCertificate = getOurCertificate ();
+    return ourCertificate.getSerialNumber ().equals (candidate.getSerialNumber ());
+  }
 
-        if (keystorePassword == null) {
-            throw new IllegalStateException("Keystore password not specified");
-        }
+  public static void setKeystoreLocation (final String keystoreLocation) {
+    KeystoreManager.keystoreLocation = keystoreLocation;
+  }
 
-        if (!keystoreFile.exists()) {
-            throw new IllegalStateException("Keystore file " + keystoreFile + " does not exist");
-        }
-
-        try {
-
-            setKeystoreLocation(keystoreFile.getCanonicalPath());
-            setKeystorePassword(keystorePassword);
-            getKeystore();
-
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Problem accessing keystore file", e);
-        }
-    }
-
-    void initCertPathValidator() {
-        Log.debug("Initialising OCSP validator");
-
-        try {
-            TrustAnchor trustAnchor = getTrustAnchor();
-            certPathValidator = CertPathValidator.getInstance("PKIX");
-            pkixParameters = new PKIXParameters(Collections.singleton(trustAnchor));
-            pkixParameters.setRevocationEnabled(true);
-
-            Security.setProperty("ocsp.enable", "true");
-            Security.setProperty("ocsp.responderURL", "http://pilot-ocsp.verisign.com:80");
-
-        } catch (Exception e) {
-            throw new IllegalStateException("Unable to construct Certificate Path Validator; " + e, e);
-        }
-    }
-
-    /**
-     * Validates a X509 certificate using the OCSP services.
-     *
-     * @param certificate the certificate to be checked for validity
-     * @return <code>true</code> if certificate is valid, <code>false</code> otherwise
-     */
-    public synchronized boolean validate(X509Certificate certificate) {
-
-        BigInteger serialNumber = certificate.getSerialNumber();
-        String certificateName = "Certificate " + serialNumber;
-        Log.debug("Ocsp validation requested for " + certificateName + "\n\tSubject:" + certificate.getSubjectDN() + "\n\tIssued by:" + certificate.getIssuerDN());
-
-        if (certPathValidator == null) {
-            throw new IllegalStateException("Certificate Path validator not initialized");
-        }
-
-        if (ocspValidatorCache.isKnownValidCertificate(serialNumber)) {
-            Log.debug(certificateName + " is OCSP valid (cached value)");
-            return true;
-        }
-
-        try {
-
-            List<Certificate> certificates = Arrays.asList(new Certificate[]{certificate});
-            CertPath certPath = CertificateFactory.getInstance("X.509").generateCertPath(certificates);
-            certPathValidator.validate(certPath, pkixParameters);
-            ocspValidatorCache.setKnownValidCertificate(serialNumber);
-
-            Log.debug(certificateName + " is OCSP valid");
-            return true;
-
-        } catch (Exception e) {
-            Log.error(certificateName + " failed OCSP validation", e);
-            return false;
-        }
-    }
-
-    public boolean isOurCertificate(X509Certificate candidate) {
-        X509Certificate ourCertificate = getOurCertificate();
-        return ourCertificate.getSerialNumber().equals(candidate.getSerialNumber());
-    }
-
-    public static void setKeystoreLocation(String keystoreLocation) {
-        KeystoreManager.keystoreLocation = keystoreLocation;
-    }
-
-    public static void setKeystorePassword(String keystorePassword) {
-        KeystoreManager.keystorePassword = keystorePassword;
-    }
+  public static void setKeystorePassword (final String keystorePassword) {
+    KeystoreManager.keystorePassword = keystorePassword;
+  }
 }
