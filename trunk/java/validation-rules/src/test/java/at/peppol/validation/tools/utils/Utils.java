fb2727c9570360e542fe80469e28e2976dc5bdef
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-08-02 17:50:44 +0000

    Added no_reminder and feature complete for generation purposes

--- trunk/java/validation-rules/src/test/java/at/peppol/validation/tools/utils/Utils.java
+++ trunk/java/validation-rules/src/test/java/at/peppol/validation/tools/utils/Utils.java
@@ -3,8 +3,13 @@ package at.peppol.validation.tools.utils;
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Immutable
 public final class Utils {
+  private static final Logger s_aLogger = LoggerFactory.getLogger (Utils.class);
+
   private Utils () {}
 
   @Nonnull
@@ -13,6 +18,10 @@ public final class Utils {
   }
 
   public static void log (final String s) {
-    System.out.println (s);
+    s_aLogger.info (s);
+  }
+
+  public static void warn (final String s) {
+    s_aLogger.warn (s);
   }
 }
