b06c54d6a0d3c2ccab9696d72c044aca38cf7a67
ph@phloc.com <ph@phloc.com@d2946293-3d23-9d3c-7509-a8ee335a6808>
2012-08-02 07:50:11 +0000

    Updated

---
+++ trunk/java/validation-rules/src/test/java/at/peppol/validation/tools/RuleSourceCodeList.java
@@ -0,0 +1,46 @@
+package at.peppol.validation.tools;
+
+import java.io.File;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.io.file.FileOperations;
+
+public final class RuleSourceCodeList {
+  private final File m_aSourceFile;
+  private final File m_aOutputDirectory;
+  private final String m_sID;
+
+  public RuleSourceCodeList (@Nonnull final File aSourceFilename,
+                             @Nonnull final File aOutputDirectory,
+                             @Nonnull @Nonempty final String sID) {
+    if (!aSourceFilename.isFile ())
+      throw new IllegalArgumentException ("Source file does not exist: " + aSourceFilename);
+    FileOperations.createDirIfNotExisting (aOutputDirectory);
+    m_aSourceFile = aSourceFilename;
+    m_aOutputDirectory = aOutputDirectory;
+    m_sID = sID;
+  }
+
+  @Nonnull
+  public File getSourceFile () {
+    return m_aSourceFile;
+  }
+
+  @Nonnull
+  public File getOutputDirectory () {
+    return m_aOutputDirectory;
+  }
+
+  @Nonnull
+  @Nonempty
+  public String getID () {
+    return m_sID;
+  }
+
+  @Nonnull
+  public File getSchematronAbstractFile (@Nonnull @Nonempty final String sTransaction) {
+    return new File (m_aOutputDirectory, m_sID + "-" + sTransaction + "-abstract.sch");
+  }
+}
